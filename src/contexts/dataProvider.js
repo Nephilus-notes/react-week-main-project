1a376e6576e1734964cfba750bfe3c9fe4b24bac
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2022-12-21 20:55:18 -0600

    added firebase connection,  about to populate using add car function

--- src/contexts/dataProvider.js
+++ src/contexts/dataProvider.js
@@ -10,10 +10,17 @@ export const DataProvider = function (props) {
 
     useEffect(() => {
         async function getCars(){
-            const response = await fetch("https://my-json-server.typicode.com/Llang8/cars-api/cars")
-            const data = await response.json()
-            setCars(data)
-            console.log(data)
+            const carsDocs = []
+            const querySnapshot = await getDocs(collection(db, 'cars'))
+
+            querySnapshot.forEach((doc) => {
+                carsDocs.push({
+                    id:doc.id,
+                    ...doc.data()
+                })
+            })
+            // console.table(carsDocs)
+            setCars(carsDocs)
         }
     
         getCars()
@@ -21,17 +28,59 @@ export const DataProvider = function (props) {
 
        
         async function loadCar(id){
-            const response = await fetch(`https://my-json-server.typicode.com/Llang8/cars-api/cars/${id}`)
-            const data = await response.json()
-           return data
+            const carRef = doc(db, 'cars', id)
+            const carSnap = await getDoc(carRef)
+
+            console.log({
+                id: carSnap.id,
+                ...carSnap.data()
+            })
+            if (carSnap.exists()) {
+                return {
+                    id: carSnap.id,
+                    ...carSnap.data()
+                }
+            } else {
+                console.log(`Car with id ${id} does not exist`)
+            }
         }
+        // name, year, selling_price, km_driven, fuel, seller_type, transmission, owner, mileage, engine, max_power, torque, seats
+            async function addCar(carObj) {
+                const newCar = carObj
+                    // name:name,
+                    // year: year,
+                    // selling_price: selling_price,
+                    // km_driven: km_driven,
+                    // fuel: fuel,
+                    // seller_type: seller_type,
+                    // transmission: transmission, 
+                    // owner: owner,
+                    // mileage: mileage,
+                    // engine: engine,
+                    // max_power, max_power,
+                    // torque: torque,
+                    // seats: seats
+                
+                console.log(` this is a new car ${newCar} `)
+                console.log(newCar)
+                // const doc = await addDoc(collection(db, 'cars'), newCar)
+
+                newCar.id = doc.id
+                setCars([newCar, ...cars])
+            
+            }
+
+
+           
+         // main context function end
 
 
 
 
     const value = {
         cars,
-        loadCar
+        loadCar, 
+        addCar
     }
 
     return (
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,37 +1,59 @@
-import { useState, useEffect } from 'react';
-import Button from '../components/Button'
-import Car from '../components/Car'
+import { useState, useEffect, useContext } from "react";
+import Button from "../components/Button";
+import { DataContext } from "../contexts/dataProvider";
+import Car from "../components/Car";
 
 export default function Profile(props) {
-    const [cars, setCars] = useState([])
-    const [car, setCar] = useState({})
-
-    useEffect(() => {
-        async function getCars(){
-            const response = await fetch("https://my-json-server.typicode.com/Llang8/cars-api/cars")
-            const data = await response.json()
-            setCars(data)
-            console.log(data)
-        }
-    
-        getCars()
-        },[])
+  const { cars, loadCar, addCar } = useContext(DataContext);
+//   const [car, setCar] = useState("");
 
 
-    return (
-        <div className="Profile">
-        <h1>Profile</h1>
-        <p>Your information is here</p>
-        <p>But it's hiding.</p>
-        <p>Click below!</p>
-        <ul>
-         {cars.map(car => <Button car={ car } handleClick={ ()=> setCar(car) } key={ car.id } showLink = 'true'/>)}
+  useEffect(() => {
+      async function buildCarDb(){
+          const response = await fetch("https://my-json-server.typicode.com/Llang8/cars-api/cars")
+          const data = await response.json()
+        //   console.log(data[1])
+          const newCar = data[1]
+console.log(newCar)
+          addCar(newCar)
+        //   console.table(data)
+      }
+
+      buildCarDb()
+      },[])
+
+  return (
+    <div className="Profile">
+      <h1>Profile</h1>
+
+      <h2>Create a New Listing</h2>
+      <form action="">
+      <div className="flex-container">
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="year" placeholder="year" />
+        <input type="text" name="selling_price" placeholder="selling_price" />
+        <input type="text" name="km_driven" placeholder="km_driven" />
+        <input type="text" name="fuel" placeholder="fuel" />
+        <input type="text" name="seller_type" placeholder="seller_type" />
+        <input type="text" name="transmission" placeholder="transmission" />
+        <input type="text" name="owner" placeholder="owner" />
+        <input type="text" name="mileage" placeholder="mileage" />
+        <input type="text" name="engine" placeholder="engine" />
+        <input type="text" name="max_power" placeholder="max_power" />
+        <input type="text" name="torque" placeholder="torque" />
+        <input type="text" name="seats" placeholder="seats" />
+        </div>
+        <button>Post it!</button>
+      </form>
+      {/* <ul>
+         {cars.map(car => <Button car={ car } handleClick={ async ()=>  await loadCar(car.id) } key={ car.id } showLink = 'true'/>)}
           {
             (car) ?
             <Car car={ car }/> :
             <></>
 
           }
-        </ul>
-        </div>
-    )}
+        </ul> */}
+    </div>
+  );
+}
